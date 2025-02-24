0100ca6ee983b3a07e40b65fe924a2d607aa90aa
Eihab Kafi <eihab6766@gmail.com>
2025-02-24 20:37:28 +0000

    main

---
+++ SpeakHello.js
@@ -0,0 +1,14 @@
+// STEP 2: 
+(function (window) {
+    var helloSpeaker = {};  
+    var speakWord = "Hello";
+  
+    // STEP 4: 
+    helloSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    };
+  
+    // STEP 5: 
+    window.helloSpeaker = helloSpeaker;
+  })(window);
+  
